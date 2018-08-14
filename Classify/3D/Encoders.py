32c55a78898373095519228b060ed1f3b74426cd
mathwizn <neeyanthkvk@gmail.com>
2018-08-13 23:18:02 -0400

    Added Encoders File

--- Classify/3D/Encoders.py
+++ Classify/3D/Encoders.py
@@ -4,7 +4,7 @@ import keras.backend as K
 from sklearn.model_selection import train_test_split
 
 # Model Creation
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.layers import Conv3D, MaxPooling3D, GlobalMaxPooling3D, GlobalAveragePooling3D
 from keras.layers import Conv2D, MaxPooling2D, GlobalMaxPooling2D, GlobalAveragePooling2D
 from keras.layers import Flatten, Dropout, Dense, Reshape, BatchNormalization, Input
@@ -27,59 +27,67 @@ encoder = (Dropout(0.25))(encoder)
 
 encoder = (Conv3D(128, padding="same", kernel_size=(5, 5, 5)))(encoder)
 encoder = (LeakyReLU())(encoder)
-encoder = (Conv3D(128, padding="same", kernel_size=(5, 5, 5)))
-model.add(LeakyReLU())
-model.add(MaxPooling3D(pool_size=(2, 2, 2), padding="same"))
-model.add(Dropout(0.25))
+encoder = (Conv3D(128, padding="same", kernel_size=(5, 5, 5)))(encoder)
+encoder = (LeakyReLU())(encoder)
+encoder = (MaxPooling3D(pool_size=(2, 2, 2), padding="same"))(encoder)
+encoder = (Dropout(0.25))(encoder)
 
-model.add(Conv3D(64, padding="same", kernel_size=(5, 5, 5)))
-model.add(LeakyReLU())
-model.add(Conv3D(64, padding="same", kernel_size=(5, 5, 5)))
-model.add(LeakyReLU())
-model.add(MaxPooling3D(pool_size=(2, 2, 2), padding="same"))
-model.add(Dropout(0.25))
+encoder = (Conv3D(64, padding="same", kernel_size=(5, 5, 5)))(encoder)
+encoder = (LeakyReLU())(encoder)
+encoder = (Conv3D(64, padding="same", kernel_size=(5, 5, 5)))(encoder)
+encoder = (LeakyReLU())(encoder)
+encoder = (MaxPooling3D(pool_size=(2, 2, 2), padding="same"))(encoder)
+encoder = (Dropout(0.25))(encoder)
 
-model.add(Conv3D(32, padding="same", kernel_size=(3, 3, 3)))
-model.add(LeakyReLU())
-model.add(Conv3D(32, padding="same", kernel_size=(3, 3, 3)))
-model.add(LeakyReLU())
-model.add(MaxPooling3D(pool_size=(2, 2, 2), padding="same"))
-model.add(Dropout(0.25))
+encoder = (Conv3D(32, padding="same", kernel_size=(3, 3, 3)))(encoder)
+encoder = (LeakyReLU())(encoder)
+encoder = (Conv3D(32, padding="same", kernel_size=(3, 3, 3)))(encoder)
+encoder = (LeakyReLU())(encoder)
+encoder = (MaxPooling3D(pool_size=(2, 2, 2), padding="same"))(encoder)
+encoder = (Dropout(0.25))(encoder)
 
-model.add(Conv3D(8, padding="same", kernel_size=(3, 3, 3)))
-model.add(LeakyReLU())
-model.add(Conv3D(8, padding="same", kernel_size=(3, 3, 3)))
-model.add(LeakyReLU())
-model.add(MaxPooling3D(pool_size=(2, 2, 2), padding="same"))
-model.add(Dropout(0.25))
+encoder = (Conv3D(8, padding="same", kernel_size=(3, 3, 3)))(encoder)
+encoder = (LeakyReLU())(encoder)
+encoder = (Conv3D(8, padding="same", kernel_size=(3, 3, 3)))(encoder)
+encoder = (LeakyReLU())(encoder)
+encoder = (MaxPooling3D(pool_size=(2, 2, 2), padding="same"))(encoder)
+encoder = (Dropout(0.25))(encoder)
 
-model.add(Flatten())
-model.add(BatchNormalization())
-model.add(Dense(128, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.5))
-model.add(Dense(2, activation='softmax'))
-model.compile(loss=categorical_crossentropy, optimizer='adam', metrics=['accuracy'])
-model.summary()
+decoder = (Conv3D(8, padding="same", kernel_size=(3, 3, 3)))(encoder)
+decoder = (LeakyReLU())(decoder)
+decoder = (UpSampling2D(pool_size=(2, 2, 2), padding="same"))(decoder)
+decoder = (Conv3D(8, padding="same", kernel_size=(3, 3, 3)))(decoder)
+decoder = (LeakyReLU())(decoder)
+decoder = (Dropout(0.25))(decoder)
 
-# Data Loading
-y = np.zeros((466+148, 2))
-x = np.concatenate([np.load("/data2/3D/PD.npy"), np.load("/data2/3D/Control.npy")])
-print("LOADED")
-for i in range(466):
-    y[i][0] = 1
-for i in range(466, 466+148):
-    y[i][1] = 1
-print("MADE Y")
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=42)
-x = 0
-y = 0
+decoder = (Conv3D(32, padding="same", kernel_size=(3, 3, 3)))(decoder)
+decoder = (LeakyReLU())(decoder)
+decoder = (UpSampling2D(pool_size=(2, 2, 2), padding="same"))(decoder)
+decoder = (Conv3D(32, padding="same", kernel_size=(3, 3, 3)))(decoder)
+decoder = (LeakyReLU())(decoder)
+decoder = (Dropout(0.25))(decoder)
 
+decoder = (Conv3D(64, padding="same", kernel_size=(5, 5, 5)))(decoder)
+decoder = (LeakyReLU())(decoder)
+decoder = (UpSampling2D(pool_size=(2, 2, 2), padding="same"))(decoder)
+decoder = (Conv3D(64, padding="same", kernel_size=(5, 5, 5)))(decoder)
+decoder = (LeakyReLU())(decoder)
+decoder = (Dropout(0.25))(decoder)
 
+decoder = (Conv3D(128, padding="same", kernel_size=(5, 5, 5)))(decoder)
+decoder = (LeakyReLU())(decoder)
+decoder = (UpSampling2D(pool_size=(2, 2, 2), padding="same"))(decoder)
+decoder = (Conv3D(128, padding="same", kernel_size=(5, 5, 5)))(decoder)
+decoder = (LeakyReLU())(decoder)
+decoder = (Dropout(0.25))(decoder)
 
-# Model Testing
-history = model.fit(x_train, y_train, validation_data = (x_test, y_test), batch_size=1, epochs = 25, verbose = 1, shuffle = True)
-import pickle
-pickle.dump(history, open("history.pkl", "wb"))
-model.save_weights("7312018.h5")
+decoder = (Conv3D(32, padding="same", kernel_size=(7, 7, 7)))(decoder)
+decoder = (LeakyReLU())(decoder)
+decoder = (UpSampling2D(pool_size=(2, 2, 2), padding="same"))(decoder)
+decoder = (Conv3D(32, padding="same", kernel_size=(7, 7, 7)))(decoder)
+decoder = (LeakyReLU())(decoder)
+decoder = (Dropout(0.25))(decoder)
 
+decoder = (Conv3D(1, padding="same", kernel_size=(7, 7, 7)))(decoder)
+
+autoencoder = Model(input_img, decoder)

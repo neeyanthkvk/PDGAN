fef9e3daa7d99010db1ebe6f742ef2392db3ef2c
mathwizn <neeyanthkvk@gmail.com>
2018-08-26 16:44:02 -0400

    Finished First GAN

--- Generation/GAN1.py
+++ Generation/GAN1.py
@@ -12,7 +12,20 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 import os
+from PIL import Image
+import argparse
 
+'''
+Hyper-parameters
+'''
+epchs = 100
+g_lr       = 0.008
+d_lr       = 0.000001
+beta       = 0.5
+z_size     = 200
+cube_len   = 64
+obj_ratio  = 0.5
+shap = (176, 256, 240, 1)
 
 def generator(phase_train=True, params={'latent_dim':200, 'strides':(2,2,2), 'kernel_size':(4,4,4)}):
     l_dim = params['latent_dim']
@@ -77,30 +90,29 @@ def discriminator(phase_train = True, params={'shape':64, 'strides':(2,2,2), 'ke
     discrims.summary()
     return discrims
 
-if __name__ == "__main__":
-    shap = (176, 256, 240, 1)
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", type=str)
+    parser.add_argument("--batch_size", type=int, default=4)
+    parser.add_argument("--train", dest="train", action="store_true")
+    parser.add_argument("--epochs", type=int, default=10)
+    parser.set_defaults(train=False)
+    args = parser.parse_args()
+    return args
+
+def train(b_size, epchs):
+    (x_train, y_train), (x_test, y_test) = (1,1), (1,1)
+    x_train = (x_train.astype(np.float32) - 127.5)/127.5 #Scales from -1 to 1
+    x_train = x_train[:,:,:,:,None]
+    x_test = x_test[:,:,:,:,None]
+
     gen = generator(True, {'latent_dim': 1000, 'strides':(2,2,2), 'kernel_size':(5,5,5)})
     disc = discriminator(params={'shape':shap, 'strides':(2,2,2), 'kernel_size':(5,5,5), 'leak_value':0.25})
 
-    '''
-    Hyper-parameters
-    '''
-    epchs = 100
-    batch_size = 1
-    g_lr       = 0.008
-    d_lr       = 0.000001
-    beta       = 0.5
-    z_size     = 200
-    cube_len   = 64
-    obj_ratio  = 0.5
-
-    train_dir = ""
-    model_dir = ""
-
-    model = Sequential()
-    model.add(gen)
+    finModel = Sequential()
+    finModel.add(gen)
     disc.trainable = False
-    model.add(disc)
+    finModel.add(disc)
 
     g_optimizer = Adam(lr = g_lr, beta_1 = beta)
     d_optimizer = Adam(lr = d_lr, beta_1 = beta)
@@ -109,3 +121,38 @@ if __name__ == "__main__":
     model.compile(loss = 'binary_crossentropy', optimizer = g_optimizer)
     disc.trainable = True
     disc.compiler(loss = 'binary_crossentropy', optimizer = d_optimizer)
+
+    for epoch in range(epchs):
+        num_steps = int(X_train.shape[0]/b_size)
+        for index in range(num_steps):
+            noise = np.random.uniform(-1, 1, size=(b_size, 1000))
+            image_batch = x_train[index*b_size:(index+1)*b_size]
+            gen_img = gen.predict(noise, verbose = 0)
+            X = np.concatenate((image_batch, generated_images))
+            y = [1] * b_size + [0] * b_size
+            d_l = disc.train_on_batch(X, y)
+            noise = np.random.uniform(-1, 1, size=(b_size, 1000))
+            disc.trainable = False
+            g_loss = model.train_on_batch(noise, [1] * BATCH_SIZE)
+            d.trainable = True
+            if index % 10 == 9:
+                g.save_weights('generator', True)
+                d.save_weights('discriminator', True)
+
+def gen(b_size, epchs):
+    gen = generator(True, {'latent_dim': 1000, 'strides':(2,2,2), 'kernel_size':(5,5,5)})
+    g.compile(loss='binary_crossentropy', optimizer="SGD")
+    g.load_weights('generator')
+    noise = np.random.uniform(-1, 1, (BATCH_SIZE, 100))
+    generated_images = g.predict(noise, verbose=1)
+    image = combine_images(generated_images)
+    image = image*127.5+127.5
+    Image.fromarray(image.astype(np.uint8)).save(
+        "generated_image.png")
+
+if __name__ == "__main__":
+    args = get_args()
+    if args.mode == "train":
+        train(b_size = args.batch_size, epchs = args.epoch)
+    elif args.mode == "generate":
+        gen(b_size = args.batch_size, epchs = args.epoch)

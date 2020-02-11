9caa41e4665c060c24a96c587e644a4cd1edf6bc
Neeyanth Kopparapu <neeyanthkvk@gmail.com>
2020-02-10 21:27:31 -0500

    Even Even Even Even Even More Bug Fixes

--- mlopt/classifier.py
+++ mlopt/classifier.py
@@ -28,7 +28,7 @@ class Classifier:
 
         print("**Evaluation Information** \n")
         if(self.training_complete):
-            print("Model Performance: " + self.metrics)
+            print("Model Performance: " + str(self.metrics))5
             print("Number of Training Epochs Completed: " + self.epochs)
 
     def split_data(self, split_ratio = 0.2):
@@ -37,7 +37,7 @@ class Classifier:
         self.train_data, self.test_data, self.train_labels, self.test_labels = train_test_split(self.X, self.y,
                                                                                                 random_state = self.random_seed,
                                                                                                 test_size = self.data_split)
-        print("Split Successful: Splitting with the Ratio of: " + self.data_split)
+        print("Split Successful: Splitting with the Ratio of: " + str(self.data_split))
 
     def set_num_folds(self, num_folds = 8):
         self.num_folds = num_folds
@@ -52,9 +52,9 @@ class Classifier:
 
     def train(self):
         if(self.data_split is None):
-            raise Error("Data Split Not Defined")
+            raise Exception("Data Split Not Defined")
         if(self.trained):
-            raise Error("Already Trainined")
+            raise Exception("Already Trainined")
         self.curr_model = self.create_model()
         if(self.num_folds is not None):
             self.training_results = {}
@@ -62,7 +62,7 @@ class Classifier:
             for counter, train_index, test_index in enumerate(kf.split(self.train_data)):
                 X_train_split, X_test_split = self.train_data[train_index], self.train_data[test_index]
                 y_train_split, y_test_split = self.train_labels[train_index], self.train_labels[test_index]
-                y_acc, y_our = single_training_cycle(X_train_split, y_train_split, X_test_split, y_test_split)
+                y_acc, y_our = self.single_training_cycle(X_train_split, y_train_split, X_test_split, y_test_split)
                 for metric in self.evaluation_metrics:
                     if metric.__name__ not in self.training_results:
                         self.training_results[metric.__name__] = []
@@ -70,16 +70,16 @@ class Classifier:
                     self.training_results[metric.__name__].append(metric(y_acc, y_our))
         else:
             self.training_results = {}
-            y_acc, y_our = single_training_cycle(self.train_data, self.train_labels, self.test_data, self.test_labels)
+            y_acc, y_our = self.single_training_cycle(self.train_data, self.train_labels, self.test_data, self.test_labels)
             for metric in self.evaluation_metrics:
                 self.training_results[metric.__name__] = metric(y_acc, y_our)
         self.trained = True;
 
     def evaluate(self, save = False, save_dir = None):
         if(self.trained is None):
-            raise Error("Model not Trained")
+            raise Exception("Model not Trained")
         self.testing_results = {}
-        y_acc, y_our = single_training_cycle(self.train_data, self.train_labels, self.test_data, self.test_labels, save = save, save_dir = save_dir)
+        y_acc, y_our = self.single_training_cycle(self.train_data, self.train_labels, self.test_data, self.test_labels, save = save, save_dir = save_dir)
         for metric in self.evaluation_metrics:
             self.testing_results[metric.__name__] = metric(y_acc, y_our)
 
@@ -92,22 +92,20 @@ class Classifier:
         if(self.random_seed is not None):
             d["random_seed"] = self.random_seed
         if(self.metrics is not None):
-            d["metrics"] = [metric.__name__ for metric in metrics]
+            d["metrics"] = [metric.__name__ for metric in self.metrics]
         df = pd.DataFrame(data = d)
         df.to_csv(os.path.join(dir_name, "options.csv"))
-        with open(os.path.join(dir_name, 'metrics.pkl'), 'wb') as handle:
-                pickle.dump(d, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     def get_test_metric(self, metric):
         if(self.testing_results is None):
-            raise Error("Model not Evaluated")
+            raise Exception("Model not Evaluated")
         if(metric.__name__ not in self.testing_results):
-            raise Error("Metric not Evaluated")
+            raise Exception("Metric not Evaluated")
         return self.testing_results[metric.__name__]
 
     def get_training_metric(self, metric):
         if(self.training_results is None):
-            raise Error("Model not Trained")
+            raise Exception("Model not Trained")
         if(metric.__name__ not in self.training_results):
-            raise Error("Metric not Trained")
+            raise Exception("Metric not Trained")
         return self.testing_results[metric.__name__]
